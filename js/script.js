73bc9b3c4d1f92713026087e63e48679e1289334
CarlosBF <cborrellf@outlook.es>
2025-03-17 10:11:29 +0100

    Solution 2 with filter

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 import peliculas from './peliculas.js'
-
+/*
 const actionContainer = document.getElementById('genero-28')
 const thrillerContainer = document.getElementById('genero-53')
 const adventureContainer = document.getElementById('genero-12')
@@ -18,7 +18,7 @@ function createFilm(film) {
     element.appendChild(title)
 
     if(film.genre_ids.indexOf(28) != -1){
-        actionContainer.append(element.cloneNode(true))
+        actionContainer.appendChild(element.cloneNode(true))
     }
     if(film.genre_ids.indexOf(53) != -1){
         thrillerContainer.appendChild(element.cloneNode(true))
@@ -29,3 +29,41 @@ function createFilm(film) {
 }
 
 peliculas.map((film) => createFilm(film))
+*/
+
+/* LIVE REVIEW */
+
+/* Variables DOM */
+const actionContainer = document.getElementById('genero-28')
+const thrillerContainer = document.getElementById('genero-53')
+const adventureContainer = document.getElementById('genero-12')
+/* Inicio URL imágenes */
+const imgURL = 'https://image.tmdb.org/t/p/w500'
+/* Filtrado de películas */
+const actionFilms = peliculas.filter((film) => film.genre_ids.includes(28))
+const thrillerFilms = peliculas.filter((film) => film.genre_ids.includes(53))
+const adventureFilms = peliculas.filter((film) => film.genre_ids.includes(12))
+
+actionFilms.forEach((film) => {
+    actionContainer.appendChild(createFilm(film))
+})
+thrillerFilms.forEach((film) => {
+    thrillerContainer.appendChild(createFilm(film))
+})
+adventureFilms.forEach((film) => {
+    adventureContainer.appendChild(createFilm(film))
+})
+
+function createFilm(film) {
+    let element = document.createElement('a')
+    element.setAttribute('class', 'film')
+    element.setAttribute('href', imgURL+film.backdrop_path)
+    element.setAttribute('target', '_blank')
+    let image = document.createElement('img')
+    image.setAttribute('src', imgURL+film.poster_path)
+    element.appendChild(image)
+    let title = document.createElement('h4')
+    title.innerText = film.title
+    element.appendChild(title)
+    return element
+}
